3f0e4bcbabe3780f7a6bc9dab8047257a533dfee
Jenna Jordan <jenna.jrdn@gmail.com>
2021-11-29 13:51:21 -0500

    add documentation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Boston_CityScore_App
 Streamlit app visualizing the CityScore metrics curated by Analyze Boston
+
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/jenna-jordan/boston_cityscore_app/main/app.py)
+
+## About the Streamlit app
+This app is a visual exploration of Boston's CityScore. You can look at the current reported scores, descriptions of each of the metrics, charts of historical scores, as well as getting the raw data.
+    
+### Navigation
+
+Use the menu options in the sidebar to the left to navigate the app.
+
+### Data Source
+
+This app uses the [CityScore Full Metric List](https://data.boston.gov/dataset/cityscore/resource/dd657c02-3443-4c00-8b29-56a40cfe7ee4) dataset from [Analyze Boston](https://data.boston.gov).
+Specifically, the data is accessed via the CKAN Data API, so the app will always display the latest data.
+
+### Code
+
+You can find the code used to create this app on the [GitHub repo](https://github.com/jenna-jordan/Boston_CityScore_App).
+
+This app was last updated on 2021 November 29.
+
--- app.py
+++ app.py
@@ -95,9 +95,10 @@ df = enhance_dataframe(df)
 # set up sidebar nav
 with st.sidebar:
     st.subheader("Navigation")
-    menu = st.selectbox(
+    menu = st.radio(
         "What would you like to see?",
         [
+            "About this App",
             "Current Scores",
             "About the Metrics",
             "Historical Scores",
@@ -106,7 +107,32 @@ with st.sidebar:
     )
 
 st.header(menu)
-if menu == "Current Scores":
+if menu == "About this App":
+    st.markdown(
+        """
+    This app is a visual exploration of Boston's CityScore. You can look at the current reported scores, descriptions of each of the metrics, charts of historical scores, as well as getting the raw data.
+    
+    ### Navigation
+
+    Use the menu options in the sidebar to the left to navigate the app.
+
+    ### Data Source
+
+    This app uses the [CityScore Full Metric List](https://data.boston.gov/dataset/cityscore/resource/dd657c02-3443-4c00-8b29-56a40cfe7ee4) dataset from [Analyze Boston](https://data.boston.gov).
+    Specifically, the data is accessed via the CKAN Data API, so the app will always display the latest data.
+
+    ### Code
+
+    You can find the code used to create this app on the [GitHub repo](https://github.com/jenna-jordan/Boston_CityScore_App).
+    
+    This app was last updated on 2021 November 29.
+    
+    ### About the creator
+
+    Hi! My name is Jenna Jordan, and I created this Streamlit app. You can learn more about me at my website, [jennajordan.me](https://jennajordan.me)
+    """
+    )
+elif menu == "Current Scores":
     df_current_scores = df[df["latest_score_flag"] == True]
     df_current_scores = (
         df_current_scores[
